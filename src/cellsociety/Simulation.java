15ca69fd8ad1237ef39f252be1e1f055c358e19d
Karthik Ramachandran <karthik.ramachandran@duke.edu>
2020-02-03 00:57:53 -0500

    implemented percolation

--- src/cellsociety/Simulation.java
+++ src/cellsociety/Simulation.java
@@ -5,11 +5,24 @@ import java.util.List;
 
 public abstract class Simulation {
   private Model myModel;
+  ArrayList<ArrayList<Cell>> myGrid;
+
+  public Simulation(Model model) {
+    this.myModel = model;
+    myGrid = myModel.getGrid();
+  }
   public void run(){
     findNewStates();
     setNewStates();
   }
   protected abstract ArrayList<Cell> getNeighbors(Cell cell);
   protected abstract void findNewStates();
-  protected abstract void setNewStates();
+
+  private void setNewStates() {
+    for(int i = 0; i < myGrid.size(); i++) {
+      for (int j = 0; j < myGrid.get(i).size(); j++) {
+        myGrid.get(i).get(j).updateState();
+      }
+    }
+  }
 }
